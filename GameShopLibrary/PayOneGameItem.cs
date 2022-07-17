8c6dcffb523eed6ef7a8ce2b03da14ffaeaf80ee
Zipizx <Zipizx@outlook.com>
2022-07-17 23:39:25 +0300

    Working with the constructor of the PayOneGameItem class

--- GameShopLibrary/PayOneGameItem.cs
+++ GameShopLibrary/PayOneGameItem.cs
@@ -5,7 +5,10 @@ namespace GameShopLibrary
 {
     class PayOneGameItem : Item
     {
+        public PayOneGameItem(string name,int coast,Guid Id) : base (name,coast,Id)
+        {
 
+        }
 
     }
 }
