3dc55d74eece2f8eea229b958b772e0aba159b43
Zipizx <Zipizx@outlook.com>
2022-07-20 00:37:21 +0300

    Creating a User controller and adding requests

---
+++ GameShopWeb/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameShopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Route("GameShop/user/allgame")]
+        [HttpGet]
+        public void AllGame()
+        { 
+        }
+        [Route("GameShop/user/buygames")]
+        [HttpGet]
+        public void BuyGame()
+        { 
+        }
+        [Route("GameShop/user/buygames")]
+        [HttpPost]
+        public void ChangeName(string newName)
+        { 
+        }
+
+
+
+
+
+
+
+
+    }
+}
