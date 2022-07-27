46b2a25e3bb5856552fc318113a98fa034b60a79
Zipizx <Zipizx@outlook.com>
2022-07-27 12:42:51 +0300

    Creating a facade for the Item and User class

---
+++ GameShopLibrary/ItemFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameShopLibrary
+{
+    interface IItemFacade
+    {
+        public void SetPrice(double cost);
+
+        public void SetId(Guid id);
+
+        public void SetGame(Game game);
+
+    }
+}
---
+++ GameShopLibrary/UserFacade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameShopLibrary
+{
+    interface IUserFacade
+    {
+        public void BuyItem(Guid idGame);
+
+        public void ChangeNickName(string newNickName);
+
+        public void SelectPaymentOption(PaymentOption paymentOption);
+
+        public List<Item> ShowAllItem();
+
+        public void ChangePayment(PaymentOption paymentOption);
+
+    }
+}
